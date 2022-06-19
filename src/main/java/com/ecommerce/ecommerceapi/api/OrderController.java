89e6e1ace32ff7d792a836ac01adc7927df5bd5b
DESKTOP-OJ1GR5E\HP <kasimoluwasegun@gmail.com>
2022-06-19 12:27:07 +0100

    added Order Controller

---
+++ src/main/java/com/ecommerce/ecommerceapi/api/OrderController.java
@@ -0,0 +1,10 @@
+package com.ecommerce.ecommerceapi.api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/cart")
+@RestController
+public class OrderController {
+}
